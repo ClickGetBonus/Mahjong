5891d5670ed3d000cd46b90fc3008c80357abb43
admin <zhuofeng.jia@condenast.com.cn>
2017-04-20 16:48:33 +0800

    Initial Commit

---
+++ Mahjong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mahjong
+//
+//  Created by 贾卓峰 on 2017/4/20.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
